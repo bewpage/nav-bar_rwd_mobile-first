f45aca1fe0d0b6b5547eccfbd852dd17efea5309
bewpage <b.jedlinski@bewpage.com>
2017-06-09 12:08:09 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
         var nav = $('.header__main-nav');
         var mobileNav = $('.toggle-nav');
         var dropDownMenu = $('.header__responsive-menu');
+        var hamburgerAnimation = $('#nav-icon1');
 
     mobileNav.on('click', function(e) {
         dropDownMenu.toggleClass('expand');
+        mobileNav.toggleClass('toggle-nav-click');
+        hamburgerAnimation.toggleClass('open')
         e.preventDefault();
     });
 
