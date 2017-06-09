f7bbf1dc471373d5460bcddeaea92b4505ae2aa1
bewpage <b.jedlinski@bewpage.com>
2017-06-09 15:19:54 +0200

    update

--- js/script.js
+++ js/script.js
@@ -14,4 +14,49 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
+    // hide nav bar when scroll
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = nav.outerHeight();
+    console.log(navbarHeight);
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        console.log('to jest st= ' + st);
+        console.log('to jest lastscrolltop= ' + lastScrollTop);
+
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').removeClass('f-nav-fix').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up').addClass('f-nav-fix');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+
 });
