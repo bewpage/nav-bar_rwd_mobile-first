0b473fcef7139bdb9396c0359ae1fec78026c9a1
bewpage <b.jedlinski@bewpage.com>
2017-06-22 12:13:36 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
         var nav = $('.header__main-nav');
         var bgNav = $('.header__menu-fix');
-        var mobileNav = $('.toggle-nav');
+        var mobileNav = $('#nav-icon1');
         var dropDownMenu = $('.header__responsive-menu');
         var hamburgerAnimation = $('#nav-icon1');
+        var hamburgerNav = $('.nav-icon');
 
     mobileNav.on('click', function(e) {
         dropDownMenu.toggleClass('expand');
@@ -48,13 +49,19 @@ document.addEventListener("DOMContentLoaded", function() {
         // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight){
             // Scroll Down
+            $('nav').removeClass('header__main-nav');
             $('nav').removeClass('f-nav-fix').addClass('nav-up');
             bgNav.removeClass('b-nav-fix');
+            hamburgerNav.removeClass('b-nav-fix');
+            hamburgerNav.removeClass('f-nav-mobile-fix');
 
             //scroll up
         } else if ((st + $(window).height() < $(document).height())){
                 $('nav').removeClass('nav-up').addClass('f-nav-fix');
                 bgNav.addClass('b-nav-fix');
+                hamburgerAnimation.css('float', 'none');
+                // hamburgerAnimation.addClass('nav-icon-test');
+                hamburgerNav.addClass('f-nav-mobile-fix');
             }
 
         lastScrollTop = st;
@@ -63,6 +70,12 @@ document.addEventListener("DOMContentLoaded", function() {
         if (st === 0){
             $('nav').removeClass('nav-up').removeClass('f-nav-fix').addClass('header__main-nav');
             bgNav.removeClass('b-nav-fix');
+            hamburgerNav.removeClass('b-nav-fix');
+            hamburgerNav.removeClass('f-nav-mobile-fix');
+            hamburgerAnimation.attr('id','nav-icon1');
+            hamburgerAnimation.css('float', 'right');
+
+            // hamburgerAnimation.removeClass('nav-icon-test');
         }
     }
 
